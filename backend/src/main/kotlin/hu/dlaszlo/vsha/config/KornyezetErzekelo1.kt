b75f03a64f6535d902ecc7384d381ba23590f18d
László Dávid <laszlo.david@gmail.com>
2019-12-01 01:45:39 +0100

    Könyvtár frissítés Környezet érzékelő

---
+++ backend/src/main/kotlin/hu/dlaszlo/vsha/config/KornyezetErzekelo1.kt
@@ -0,0 +1,60 @@
+package hu.dlaszlo.vsha.config
+
+import hu.dlaszlo.vsha.device.AbstractDeviceConfig
+import org.influxdb.dto.Point
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
+
+@Component("kornyezetErzekelo1")
+class KornyezetErzekelo1 : AbstractDeviceConfig() {
+
+    data class DeviceState(
+        val mqttName: String = "ambs1",
+        val name: String = "Környezet érzékelő ($mqttName)"
+    )
+
+    var state = DeviceState()
+
+
+    override var device = device {
+
+        subscribe {
+            topic = "${state.mqttName}"
+            handler = { payload ->
+                logger.info(payload)
+
+                val pressure: Double = jsonValue(payload, "$.pressure")!!
+                val gasResistance: Double = jsonValue(payload, "$.gasResistance")!!
+                val iaq: Double = jsonValue(payload, "$.iaq")!!
+                val iaqAccuracy: Int = jsonValue(payload, "$.iaqAccuracy")!!
+                val temperature: Double = jsonValue(payload, "$.temperature")!!
+                val humidity: Double = jsonValue(payload, "$.humidity")!!
+                val dewPointTemperature = calculateDewPoint(humidity, temperature)
+
+                logger.info(
+                    "Légköri nyomás: {}, Gáz ellenállás: {}, Levegő minőség: {}, Levegő minőség pontossága: {}, Hőmérséklet: {}, Relatív páratartalom: {}, Harmatponti hőmérséklet: {}",
+                    pressure, gasResistance, iaq, iaqAccuracy, temperature, humidity, dewPointTemperature
+                )
+
+                influxDB.write(
+                    Point.measurement("ambs1")
+                        .time(currentTime(), TimeUnit.MILLISECONDS)
+                        .addField("pressure", pressure)
+                        .addField("gasResistance", gasResistance)
+                        .addField("iaq", iaq)
+                        .addField("iaqAccuracy", iaqAccuracy)
+                        .addField("temperature", temperature)
+                        .addField("humidity", humidity)
+                        .addField("dewPointTemperature", dewPointTemperature)
+                        .build()
+                )
+            }
+        }
+
+    }
+
+    companion object {
+        val logger = LoggerFactory.getLogger(KornyezetErzekelo1::class.java)!!
+    }
+}

cc1f18b71eb68ed267382258b5ff7eb4e8c99ee7
László Dávid <laszlo.david@gmail.com>
2020-02-12 23:34:57 +0100

    Gradle 6.1.1 + a com.graphql-java-kickstart használata

--- backend/src/main/kotlin/hu/dlaszlo/vsha/Configuration.kt
+++ backend/src/main/kotlin/hu/dlaszlo/vsha/Configuration.kt
@@ -1,5 +1,8 @@
 package hu.dlaszlo.vsha
 
+import graphql.scalars.ExtendedScalars
+import graphql.schema.GraphQLScalarType
+import graphql.servlet.apollo.ApolloScalars
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.task.TaskExecutor
@@ -17,6 +20,21 @@ class Configuration {
         executor.threadNamePrefix = "main_executor"
         executor.initialize()
         return executor
+    }
+
+    @Bean
+    fun upload(): GraphQLScalarType {
+        return ApolloScalars.Upload
+    }
+
+    @Bean
+    fun date(): GraphQLScalarType {
+        return ExtendedScalars.Date
+    }
+
+    @Bean
+    fun dateTime(): GraphQLScalarType {
+        return ExtendedScalars.DateTime
     }
 
 }

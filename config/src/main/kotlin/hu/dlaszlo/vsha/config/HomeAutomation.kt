8dbc5ffc5792f11916c46edf0117ee8654ccfc5f
dlaszlo <laszlo.david@gmail.com>
2020-08-07 22:27:20 +0200

    A backend és a config szétválasztása

--- config/src/main/kotlin/hu/dlaszlo/vsha/config/HomeAutomation.kt
+++ config/src/main/kotlin/hu/dlaszlo/vsha/config/HomeAutomation.kt
@@ -3,7 +3,6 @@ package hu.dlaszlo.vsha.config
 import hu.dlaszlo.vsha.backend.BackendConfiguration
 import hu.dlaszlo.vsha.backend.HomeAutomationService
 import org.slf4j.LoggerFactory
-import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.annotation.Import
@@ -16,12 +15,7 @@ class HomeAutomation
 val logger = LoggerFactory.getLogger(HomeAutomation::class.java)!!
 
 fun main(args: Array<String>) {
-
-    val context = SpringApplicationBuilder(HomeAutomation::class.java)
-        .web(WebApplicationType.NONE)
-        .run(*args)
-
+    val context = SpringApplicationBuilder(HomeAutomation::class.java).run(*args)
     context.getBean(HomeAutomationService::class.java).run()
-
 }
 

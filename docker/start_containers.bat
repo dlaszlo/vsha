d2a7a332c298324d8c82a931339194033626560f
dlaszlo <laszlo.david@gmail.com>
2020-08-12 23:01:13 +0200

    Az Nginx biztosítsa a React frontend-et, mint statikus tartalmat

--- docker/start_containers.bat
+++ docker/start_containers.bat
@@ -4,10 +4,19 @@ cd ..
 call gradlew clean build --exclude-task test
 IF ERRORLEVEL 1 GOTO FINISH
 
-cd docker
+cd frontend
+call npm run build
+IF ERRORLEVEL 1 GOTO FINISH
+
+cd ..\docker
 del .\homeautomation\vsha.jar
 copy ..\config\build\libs\vsha.jar .\homeautomation\vsha.jar
 IF ERRORLEVEL 1 GOTO FINISH
+
+cd nginx
+rmdir /S /Q .\www
+robocopy ..\..\frontend\build .\www /E
+cd ..
 
 docker-compose down
 IF ERRORLEVEL 1 GOTO FINISH
